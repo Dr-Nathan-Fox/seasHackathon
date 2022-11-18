69631fff9f73d2e4e3b7faf44a9832e5c2ce54c4
Nathan Fox <nf2g13@soton.ac.uk>
2022-11-18 14:59:30 -0500

    - update wordclouds

--- R/reddit_comment.R
+++ R/reddit_comment.R
@@ -22,41 +22,22 @@ for (i in 1:nrow(reddit_data)){
 }
 
 
+reddit_comments <- reddit_data
+
 # 4. Text analysis --------------
 r_text <- reddit_comments %>%
-  unnest_tokens(word, comment) %>%
+  unnest_tokens(word, title) %>%
   anti_join(stop_words)
 
 r_text <- data.frame(table(unlist(r_text$word)))
 
-#order by number of favourites
-r_text <- r_text[with(r_text, order(-Freq)),]
 
 #keep top 100 to match reddit data
-r_text <- r_text[1:100,]
+r_text <- subset(r_text, Freq > 25)
 
-r_text$Freq <- 1
-
-set.seed(420024)
-wordcloud2(r_text, size = .15,
-           rotateRatio = 0)
-
-
-f_text <- flickr_comments %>%
-  unnest_tokens(word, comment) %>%
-  anti_join(stop_words)
-
-f_text <- data.frame(table(unlist(f_text$word)))
-
-#order by number of favourites
-f_text <- f_text[with(f_text, order(-Freq)),]
-
-#keep top 100 to match reddit data
-f_text <- f_text[1:100,]
-
-f_text$Freq <- 1
-
+#r_text$Freq <- 1
 
 set.seed(420024)
-wordcloud2(f_text, size = .15,
+wordcloud2(r_text, size = 10,
            rotateRatio = 0)
+
